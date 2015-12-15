851dfcd82d72aab923e8a2541453e5219363fef0
Nicholas Rivali <nicholasrivali@gmail.com>
2015-12-15 11:04:23 -0600

    Average test scores

---
+++ ex2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input = 0;
+            double total = 0;
+            double average = 0;
+            double nums = 0;
+
+            Console.WriteLine("Enter in your test scores for the average");
+            Console.WriteLine("Enter 111 when you're finished");
+            while (input != 111)
+            {
+                input = Convert.ToDouble(Console.ReadLine());
+                if (input <= 100 && input >= 0)
+                {
+                    Console.WriteLine("OK");
+                    total += input;
+                    ++nums;
+                    average = total / nums;
+                }
+                else
+                {
+                    Console.WriteLine("You took " + nums + " tests");
+                    Console.WriteLine("Your average is " + average);
+                }
+            }
+        }
+    }
+}
