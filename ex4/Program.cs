d807ed2f7d93155fb6f51e982f9d9fcc634bc21f
Nicholas Rivali <nicholasrivali@gmail.com>
2015-12-15 11:36:59 -0600

    Modularized Admission

---
+++ ex4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdmissionModularized
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your GPA");
+            double GPA = (Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Enter your test score");
+            double AdmissionTest = (Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine(Admission(GPA, AdmissionTest));
+        }
+        private static string Admission(double GPA, double admissionTest)
+        {
+            string message = "";
+            if ((GPA >= 3.0 && admissionTest >= 60) || (GPA < 3.0 && admissionTest >= 80))
+            {
+                message = "Accepted";
+            }
+            else
+            {
+                message = "Rejected";
+            }
+
+            return message;
+
+        }
+    }
+}
