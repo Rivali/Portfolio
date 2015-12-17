5e152c6c2e25591a67206825a5e998c0daa00013
Nicholas Rivali <nicholasrivali@gmail.com>
2015-12-17 01:19:22 -0600

    TestClassifiedAd

---
+++ ex6/TestClassifiedAd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassifiedAd
+{
+    class TestClassifiedAd
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Enter in your ad name");
+            string doesntMatter = Console.ReadLine();
+            Console.WriteLine("Enter in your message");
+            string input = Console.ReadLine();
+
+            ClassifiedAd ad1 = new ClassifiedAd(input);
+
+            Console.WriteLine("There are {0} words in your ad and that costs {1}", ad1.numberOfWords, ad1.price.ToString("c"));
+        }
+    }
+}
